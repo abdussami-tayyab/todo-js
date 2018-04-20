b3d4815b67f45f8edc457d892520ef5ba517faf0
Abdussami Tayyab <tayyab.abdussami@gmail.com>
2018-04-20 17:00:38 +0500

    - allow events to be removed via button click

--- js/events.js
+++ js/events.js
@@ -8,17 +8,43 @@ function Event(detail) {
 
 // add a new event
 function addEvent(detail) {
-    var event = new Event(detail);
-    events.push(event);
+    // if detail is not empty
+    if (detail !== '') {
+        var event = new Event(detail);
+        events.push(event);
+        return true;
+    }
+
+    // if detail is empty, return false
+    return false;
 }
 
 // render events list
 function renderEvents() {
+    // get events list
     var eventsList = document.getElementById('events');
     eventsList.innerHTML = '';
+
+    // add list items i.e. events to the list
     for (i = 0; i < events.length; i++) {
+        // adding event
         var event = document.createElement('li');
         event.innerHTML = events[i].detail;
+        event.className = 'list-group-item';
+
+        // adding remove button to event
+        var btnDelete = document.createElement('span');
+        btnDelete.className = 'btn-delete';
+        btnDelete.innerHTML = 'remove';
+        btnDelete.setAttribute('data-event-index', i);
+        // adding event listener for remove button
+        btnDelete.onclick = function() {
+            events.splice(this.getAttribute('data-event-index'), 1);
+            renderEvents();
+        };
+
+        // appending HTML nodes to DOM tree
+        event.appendChild(btnDelete);
         eventsList.appendChild(event);
     }
 }
@@ -29,7 +55,9 @@ btnAddEvent.onclick = function() {
     // make a new event
     var newEvent = document.getElementById('new-event');
     // add it to the events array
-    addEvent(newEvent.value);
+    if (! addEvent(newEvent.value)) {
+        alert('An event should have some detail, please try again :)');
+    }
 
     // re-render list of events
     renderEvents();
